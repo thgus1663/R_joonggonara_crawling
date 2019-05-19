e3ec7b6d77e4d21cc8fe8099dca266050def525d
Seunghui Lee <49008642+Seunghui96@users.noreply.github.com>
2019-05-19 22:36:21 +0900

    Delete taskscheduler.r

--- taskscheduler.r
+++
@@ -1,9 +0,0 @@
-install.packages("taskscheduleR")
- 
-library(taskscheduleR)
-
-taskscheduler_create(taskname = "mydailyscript", rscript = "D:\\joonggo.r", 
-                     schedule = "DAILY", starttime = "10:29", startdate = format(Sys.Date(), "%Y/%m/%d"),
-                     Rexe=file.path(Sys.getenv("R_HOME"),"bin","Rscript.exe"))
-
-taskscheduler_delete("mydailyscript")
